01485ec7b869647c57858b95eb90c3f7ed8a28a4
Valentin Kögel <valentin.koegel@consiliari.de>
2024-05-28 10:27:26 +0200

    removed unnecessary linebreaks to move related code snippets closer to each other for better readability

--- aufgabe1.go
+++ aufgabe1.go
@@ -4,7 +4,6 @@ import "fmt"
 import "time"
 
 // Example
-
 type rectangle struct {
 	length int
 	width  int
@@ -55,11 +54,9 @@ func test() {
 	var s square = square{3}
 
 	x1 := r.area() + s.area()
-
 	fmt.Printf("%d \n", x1)
 
 	x2 := sumArea(r, s)
-
 	fmt.Printf("%d \n", x2)
 
 	pt := &r
@@ -78,13 +75,10 @@ func test() {
 	// same applies to square
 
 	x4 := sumAreaScaleBefore(3, &r, &s)
-
 	fmt.Printf("%d \n", x4)
-
 }
 
 // Introducing unique function names for overloaded methods
-
 func area_Rec(r rectangle) int {
 	return r.length * r.width
 }
@@ -112,7 +106,6 @@ func scale_SqPtr(s *square, x int) {
 }
 
 // Run-time method lookup
-
 func area_Lookup(x interface{}) int {
 	var y int
 
@@ -123,7 +116,6 @@ func area_Lookup(x interface{}) int {
 		y = area_Rec(v)
 	}
 	return y
-
 }
 
 func sumArea_Lookup(x, y interface{}) int {
@@ -136,18 +128,15 @@ func test_Lookup() {
 	var s square = square{3}
 
 	x1 := area_Rec(r) + area_Sq(s)
-
 	fmt.Printf("%d \n", x1)
 
+	// rectangle <= interface{} & square <= interface{}
 	x2 := sumArea_Lookup(r, s)
-	// rectangle <= interface{}
-	// square <= interface{}
 
 	fmt.Printf("%d \n", x2)
 }
 
 // Dictionary translation
-
 type shape_Value struct {
 	val  interface{}
 	area func(interface{}) int
@@ -179,20 +168,15 @@ func test_Dict() {
 	var s square = square{3}
 
 	// 1. Plain method calls
-
 	x1 := area_Rec(r) + area_Sq(s)
-
 	fmt.Printf("%d \n", x1)
 
 	x2 := sumArea(r, s)
-
 	fmt.Printf("%d \n", x2)
 
 	pt := &r
-
-	x3 := area_Rec(*pt)
 	// Implicit conversion from pointer to value
-
+	x3 := area_Rec(*pt)
 	fmt.Printf("%d \n", x3)
 
 	//  x3 := sumAreaScaleBefore(3, r, s)
@@ -201,7 +185,6 @@ func test_Dict() {
 	// same applies to square
 
 	// 2. Calling sumArea
-
 	// Wrapper functions are needed for the following reason.
 	// (a) area_Rec has type func(rectangle) int
 	// (b) We need to store area_Rec in the "area" dictionary entry which has type func(interface{}) int
@@ -209,57 +192,44 @@ func test_Dict() {
 
 	area_Rec_Wrapper := func(v interface{}) int {
 		return area_Rec(v.(rectangle))
-
 	}
 
 	area_Sq_Wrapper := func(v interface{}) int {
 		return area_Sq(v.(square))
-
 	}
 
 	rDictShape := shape_Value{r, area_Rec_Wrapper}
-
 	sDictShape := shape_Value{s, area_Sq_Wrapper}
 
 	x4 := sumArea_Dict(rDictShape, sDictShape)
-
 	fmt.Printf("%d \n", x4)
 
 	// 3. Calling sumAreaScaleBefore
-
 	area_RecPtr_Wrapper := func(v interface{}) int {
 		return area_RecPtr(v.(*rectangle))
-
 	}
 
 	area_SqPtr_Wrapper := func(v interface{}) int {
 		return area_SqPtr(v.(*square))
-
 	}
 
 	scale_RecPtr_Wrapper := func(v interface{}, x int) {
 		scale_RecPtr(v.(*rectangle), x)
-
 	}
 
 	scale_SqPtr_Wrapper := func(v interface{}, x int) {
 		scale_SqPtr(v.(*square), x)
-
 	}
 
 	// Construct the appropriate interface values
 	rDictShapeExt := shapeExt_Value{&r, area_RecPtr_Wrapper, scale_RecPtr_Wrapper}
-
 	sDictShapeExt := shapeExt_Value{&s, area_SqPtr_Wrapper, scale_SqPtr_Wrapper}
 
 	x5 := sumAreaScaleBefore_Dict(3, rDictShapeExt, sDictShapeExt)
-
 	fmt.Printf("%d \n", x5)
 
 	// 4. Calling sumArea with a shapeExt value
-
 	x6 := sumArea_Dict(fromShapeExtToShape(rDictShapeExt), fromShapeExtToShape(sDictShapeExt))
-
 	fmt.Printf("%d \n", x6)
 }
 
